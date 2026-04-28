906c834c2a52eba32b7835af9211fadc4ca81d7f
Ramesh Kosuru <ramesh.kosuru.mastek.com>
2026-04-27 19:34:36 -0500

    first commit

---
+++ src/main/java/org/rk/cp1/Cp1Application.java
@@ -0,0 +1,13 @@
+package org.rk.cp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/rk/cp1/Cp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rk.cp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
